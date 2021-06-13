829910c70c45e6ae7fd8ee3d3cd15fbcdf7d8772
funnykoss <noanybody26@gmail.com>
2021-06-13 17:34:25 +0300

    save_checked

--- src/js/switchTheme.js
+++ src/js/switchTheme.js
@@ -1,20 +1,24 @@
 const Theme = {
   LIGHT: "light-theme",
-  DARK: "dark-theme",
+  DARK: "dark-theme"
 };
 
 const toggleTheme = document.querySelector("#theme-switch-toggle");
 const bodyRef = document.querySelector("body");
+const STORAGE_KEY = "curent-theme";
 
 bodyRef.classList.add(Theme.LIGHT);
 
 toggleTheme.addEventListener('change', onSwitchTheme);
 
+document.addEventListener('DOMContentLoaded', getLocalStorageTheme);
+
 function onSwitchTheme(event) {
     if (event.target.id !== "theme-switch-toggle") {
         return;
     }
-    bodyRef.classList.toggle(Theme.LIGHT);
+  bodyRef.classList.toggle(Theme.LIGHT);
+ 
     bodyRef.classList.toggle(Theme.DARK);
 
     onThemeSave(event);
@@ -23,15 +27,20 @@ function onSwitchTheme(event) {
 function onThemeSave(event) {
     const switchStatus = event.target.checked;
     if (switchStatus) {
-      localStorage.setItem('curent-theme', Theme.DARK)  
+      localStorage.setItem(STORAGE_KEY, Theme.DARK)  
     }
     if (!switchStatus) {
-      localStorage.setItem('curent-theme', Theme.LIGHT)  
+      localStorage.setItem(STORAGE_KEY, Theme.LIGHT)  
     }
 }
 
-const savedTheme = localStorage.getItem('curent-theme');
-if (savedTheme === Theme.DARK) {
-    toggleTheme.chacked = true;
+function getLocalStorageTheme() {
+  const saveTheme = localStorage.getItem(STORAGE_KEY);
+  if (saveTheme === Theme.DARK) {
     bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+    toggleTheme.checked = true;
+  }
 }
+
+
